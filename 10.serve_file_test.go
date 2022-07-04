b125e29e73c1f3a885004268ca260404c43ed571
Lukman Sanjaya <lukman.rocks@live.com>
2022-07-04 08:33:48 +0700

    serve file & template

---
+++ 10.serve_file_test.go
@@ -0,0 +1,54 @@
+package golang_web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/ok.html")
+	} else {
+		http.ServeFile(w, r, "./resources/notfound.html")
+	}
+}
+
+func TestServeFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourceOk string
+
+//go:embed resources/notfound.html
+var resourceNotFound string
+
+func ServeFileEmebed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourceOk)
+	} else {
+		fmt.Fprint(w, resourceNotFound)
+	}
+}
+
+func TestServerFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmebed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
