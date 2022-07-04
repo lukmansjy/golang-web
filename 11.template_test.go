6134a6e2bef8e8a5d9b8bd422781473b0441b4b3
Lukman Sanjaya <lukman.rocks@live.com>
2022-07-04 08:57:07 +0700

    update template

--- 11.template_test.go
+++ 11.template_test.go
@@ -1,6 +1,7 @@
 package golang_web
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"io"
@@ -26,6 +27,67 @@ func TestSimpleHTML(t *testing.T) {
 	recoder := httptest.NewRecorder()
 
 	SimpleHTML(recoder, request)
+
+	body, _ := io.ReadAll(recoder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func SimpleHTMLFile(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./templates/simple.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestSimpleHTMLFile(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recoder := httptest.NewRecorder()
+
+	SimpleHTMLFile(recoder, request)
+
+	body, _ := io.ReadAll(recoder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func SimpleHTMLDirectory(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseGlob("./templates/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestSimpleHTMLGlob(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recoder := httptest.NewRecorder()
+
+	SimpleHTMLDirectory(recoder, request)
+
+	body, _ := io.ReadAll(recoder.Result().Body)
+	fmt.Println(string(body))
+}
+
+// rekomendasi menggunakan golang embed
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+func TemplateEmbed(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFS(templates, "templates/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recoder := httptest.NewRecorder()
+
+	TemplateEmbed(recoder, request)
 
 	body, _ := io.ReadAll(recoder.Result().Body)
 	fmt.Println(string(body))
